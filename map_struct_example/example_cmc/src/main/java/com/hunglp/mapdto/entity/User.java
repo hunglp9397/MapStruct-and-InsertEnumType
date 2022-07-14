3de45be9d67e0507c12d5e59bba486d406b7b0d1
Hung. Le Phi - CMC Global DU3.11 <lphung@cmcglobal.vn>
2022-07-14 09:14:00 +0700

    remove uneccessary code

--- map_struct_example/example_cmc/src/main/java/com/hunglp/mapdto/entity/User.java
+++ map_struct_example/example_cmc/src/main/java/com/hunglp/mapdto/entity/User.java
@@ -23,8 +23,6 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(name = "permission")
     private Set<Permission> permissions;
-
-    @Enumerated(EnumType.STRING)
     private Gender gender;
 
 
